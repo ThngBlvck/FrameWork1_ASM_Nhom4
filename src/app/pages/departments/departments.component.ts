4deb84f6800b2ccf0c8d186d6fe4fd305d82755e
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-05-18 20:55:40 +0700

    Department, Employee

--- src/app/pages/departments/departments.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-departments',
-  template: `<router-outlet></router-outlet>`,
-})
-export class DepartmentsComponent {
-
-}
