abc56af8ec27944d655ecccce4f3f8db897102a7
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-05-18 20:56:33 +0700

    Department, Employee

---
+++ src/app/pages/departments/departments-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AddComponent } from './add/add.component';
+import { DepartmentsComponent } from './departments.component';
+import { UpdateComponent } from './update/update.component';
+import { ListComponent } from './list/list.component';
+
+
+const routes: Routes = [
+    {
+      path: '',
+      component: DepartmentsComponent,
+      children: [
+        {
+          path: 'add',
+          component: AddComponent,
+        },
+        {
+            path: 'update',
+            component: UpdateComponent,
+          },
+          {
+            path: 'list',
+            component: ListComponent,
+          },
+        
+      ],
+    },
+  ];
+  
+  @NgModule({
+    imports: [
+      RouterModule.forChild(routes),
+    ],
+    exports: [
+      RouterModule,
+    ],
+  })
+  export class DepartmentsRoutingModule {
+  }
