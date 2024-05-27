9a5e9b3ac60a000a448848a9a5d1f11ba03e4cc5
baongpc05497 <145195574+giabao204@users.noreply.github.com>
2024-05-27 12:44:17 +0700

    Efficiency and HPreports

---
+++ src/app/pages/efficiency/efficiency.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EfficiencyComponent } from './efficiency.component';
+
+describe('EfficiencyComponent', () => {
+  let component: EfficiencyComponent;
+  let fixture: ComponentFixture<EfficiencyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EfficiencyComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EfficiencyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/efficiency/efficiency.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-efficiency',
+  template: '<router-outlet></router-outlet>',
+})
+export class EfficiencyComponent {
+
+}
