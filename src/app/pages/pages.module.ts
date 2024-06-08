61f4a45ff8f2154d6a119491affa346147067848
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-06-08 20:34:23 +0700

    Fix pages.module.ts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,11 +6,7 @@ import {PagesComponent} from './pages.component';
 import {DashboardModule} from './dashboard/dashboard.module';
 import {PagesRoutingModule} from './pages-routing.module';
 import {PaginatorModule} from "../@theme/components/paginator/paginator.module";
-import { DepartmentsComponent } from './departments/departments.component';
-import { EmployeeComponent } from './employee/employee.component';
-import { AddComponent } from './departments/add/add.component';
-import { UpdateComponent } from './departments/update/update.component';
-import { ListComponent } from './departments/list/list.component';
+
 
 
 
@@ -24,11 +20,7 @@ import { ListComponent } from './departments/list/list.component';
   ],
   declarations: [
     PagesComponent,
-    DepartmentsComponent,
-    EmployeeComponent,
-    AddComponent,
-    UpdateComponent,
-    ListComponent
+
   ],
   providers: []
 })
