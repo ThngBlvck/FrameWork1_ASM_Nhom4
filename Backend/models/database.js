2d29e403b7de829f003e5ac26f9542accc3e0fcd
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2024-06-11 02:42:17 +0700

    Fix Database

--- Backend/models/database.js
+++ Backend/models/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('angular_nhom4', 'root', 'root', {
+const sequelize = new Sequelize('angular_nhom4', 'root', 'mysql', {
     host: 'localhost',
     dialect: 'mysql'
 });
